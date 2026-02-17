7ce747e1751401071272c3c4dcb0cb712ed8c101
codingworld264 <codingworld264@gmail.com>
2026-02-17 14:17:27 +0530

    Add routing and toaster

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
+import ResumeForm from './components/ResumeForm.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <ResumeForm /> },
+      { path: "resume", element: <ResumeForm /> }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
